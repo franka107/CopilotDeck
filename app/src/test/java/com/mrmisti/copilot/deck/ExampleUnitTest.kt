7e4dd91c7a6f980dd411d58981a3fae4f7153535
JeanMarcoMiranda <elecord2014@gmail.com>
2024-05-13 19:14:08 -0500

    First commit test

---
+++ app/src/test/java/com/mrmisti/copilot/deck/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.mrmisti.copilot.deck
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
